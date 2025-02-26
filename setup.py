4bc1f58df982ce1f3a8e83dd3e23f54e693d2403
ecnu_pc <821278796@qq.com>
2025-02-26 22:24:27 +0800

    add latex ai tool

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jacksung',
-    version='0.0.2.139',
+    version='0.0.2.140',
     author='Zijiang Song',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
