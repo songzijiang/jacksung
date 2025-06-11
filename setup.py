7fdbfc51dd79dcfe2f27f26778c8a711335dbf84
ecnu_pc <821278796@qq.com>
2025-06-11 21:09:06 +0800

    add ai utils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jacksung',
-    version='0.0.3.0',
+    version='0.0.3.1',
     author='Zijiang Song',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
