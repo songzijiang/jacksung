56165f9fed7d618ef43a48552d6ff849ab2295d6
ecnu_pc <821278796@qq.com>
2025-06-27 13:42:50 +0800

    Update package 0.0.3.29

--- jacksung/ai/GeoAttX.py
+++ jacksung/ai/GeoAttX.py
@@ -192,7 +192,7 @@ class GeoAttX_I(GeoAttX):
             with open(os.path.join(self.root_path, 'err.log'), 'a') as f:
                 filename = e.file_name.split(os.sep)[-1]
                 file_info = prase_filename(filename)
-                f.write(f'{e.__class__}: {file_info["start"]}\n')
+                f.write(f'{e.__class__}: {file_info["start"]} {e.file_name}\n')
             return {}
         return porcess_list
 
@@ -232,7 +232,7 @@ class GeoAttX_P(GeoAttX):
             with open(os.path.join(self.root_path, 'err.log'), 'a') as f:
                 filename = e.file_name.split(os.sep)[-1]
                 file_info = prase_filename(filename)
-                f.write(f'Not exist {file_info["start"]}\n')
+                f.write(f'Not exist {file_info["start"]} {e.file_name}\n')
             return None
 
 
@@ -273,5 +273,5 @@ class GeoAttX_M(GeoAttX):
             with open(os.path.join(self.root_path, 'err.log'), 'a') as f:
                 filename = e.file_name.split(os.sep)[-1]
                 file_info = prase_filename(filename)
-                f.write(f'Not exist {file_info["start"]}\n')
+                f.write(f'Not exist {file_info["start"]} {e.file_name}\n')
             return None
