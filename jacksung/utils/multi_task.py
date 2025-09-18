5158f73922bf93f20fd5487171f47ef27258a18a
ecnu_pc <821278796@qq.com>
2025-09-18 15:10:07 +0800

    Update package 0.0.3.71

--- jacksung/utils/multi_task.py
+++ jacksung/utils/multi_task.py
@@ -2,7 +2,6 @@ import concurrent.futures
 import threading
 import multiprocessing
 import time
-
 from tqdm import tqdm
 
 type_thread = 'type_thread'
@@ -28,27 +27,37 @@ class ThreadingLock:
         return self.owner
 
 
-def init(tl, pl):
-    global t_lock
+def init_process(pl):
+    """进程池初始化函数，只设置进程锁"""
     global p_lock
-    t_lock = tl
     p_lock = pl
+
+
+def init_thread(tl):
+    """线程池初始化函数，只设置线程锁"""
+    global t_lock
+    t_lock = tl
 
 
 class MultiTasks:
     def __init__(self, threads=10, pool=type_thread, desc="Mult. Pro."):
-        if pool == type_thread:
-            self.pool = concurrent.futures.ThreadPoolExecutor
-        elif pool == type_process:
-            self.pool = concurrent.futures.ProcessPoolExecutor
         self.threads = threads
         self.task_list = {}
         self.features = {}
         self.results = {}
-        self.thread_mutex = threading.Lock()
-        self.process_mutex = multiprocessing.Lock()
-        self.executor = self.pool(max_workers=self.threads, initializer=init,
-                                  initargs=(self.thread_mutex, self.process_mutex))
+        self.pool_type = pool
+
+        if pool == type_thread:
+            # 线程池不需要在初始化时传递锁
+            self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=self.threads)
+        elif pool == type_process:
+            # 进程池需要特殊处理锁
+            self.process_mutex = multiprocessing.Manager().Lock()
+            self.executor = concurrent.futures.ProcessPoolExecutor(
+                max_workers=self.threads,
+                initializer=init_process,
+                initargs=(self.process_mutex,)
+            )
         self.desc = desc
         self.progress_bar = None
 
@@ -62,32 +71,54 @@ class MultiTasks:
                 self.features[k] = r
 
     def wrap_fun(self, fun, args):
+        """包装函数，处理进度条更新"""
         result = fun(*args)
         if self.progress_bar:
-            self.progress_bar.update(1)
+            # 使用适当的方式更新进度条
+            if self.pool_type == type_thread:
+                # 线程环境下直接更新
+                self.progress_bar.update(1)
+            else:
+                # 进程环境下需要特殊处理，这里简化处理
+                pass
         return result
 
     def execute_task(self, print_percent=True, desc=None):
-        with self.pool(max_workers=self.threads, initializer=init,
-                       initargs=(self.thread_mutex, self.process_mutex)) as executor:
-            if print_percent:
-                self.progress_bar = tqdm(total=len(self.task_list.items()), desc=desc if desc else self.desc)
-            else:
-                self.progress_bar = None
+        if print_percent:
+            self.progress_bar = tqdm(total=len(self.task_list), desc=desc if desc else self.desc)
+
+        # 根据池类型选择不同的执行策略
+        if self.pool_type == type_thread:
+            # 线程池直接执行
             for k, f_and_a in self.task_list.items():
-                self.features[k] = executor.submit(self.wrap_fun, *(f_and_a[0], f_and_a[1]))
-            for k, feature in self.features.items():
-                self.results[k] = feature.result()
+                self.features[k] = self.executor.submit(self.wrap_fun, f_and_a[0], f_and_a[1])
+        else:
+            # 进程池需要避免传递不可序列化对象
+            for k, f_and_a in self.task_list.items():
+                # 直接提交函数，不包装进度条更新
+                self.features[k] = self.executor.submit(f_and_a[0], *f_and_a[1])
+
+        # 收集结果并更新进度条
+        for i, (k, feature) in enumerate(self.features.items()):
+            self.results[k] = feature.result()
+            if self.progress_bar and self.pool_type == type_process:
+                # 进程环境下在主线程更新进度条
+                self.progress_bar.update(1)
+
+        if self.progress_bar:
+            self.progress_bar.close()
+
         return self.results
 
 
 def worker(i):
     print(i)
     time.sleep(1)
+    return i * 2
 
 
 if __name__ == '__main__':
-    mt = MultiTasks(10)
+    mt = MultiTasks(10, pool=type_thread)
     for i in range(100):
         mt.add_task(i, worker, [i])
     mt.execute_task(print_percent=False)
