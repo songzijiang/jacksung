758c9b80de283ebd918285cf06d31738c94641f6
ecnu_pc <821278796@qq.com>
2025-09-22 18:09:36 +0800

    add ai utils

--- jacksung/utils/figure.py
+++ jacksung/utils/figure.py
@@ -152,8 +152,8 @@ def _make_fig(file_np,
     np_data = cv2.imread(save_name) - 255
     np_sum_h = np.nonzero(np_data.sum(axis=(1, 2)))[0]
     np_sum_w = np.nonzero(np_data.sum(axis=(0, 2)))[0]
-    print(np_sum_h, np_sum_w)
-    crop_png(save_name, left=np_sum_w[0], top=np_sum_h[0], right=np_sum_w[-1], bottom=np_sum_h[-1])
+    # print(np_sum_h, np_sum_w)
+    crop_png(save_name, left=np_sum_w[0], top=min(np_sum_h[0], 150), right=np_sum_w[-1], bottom=np_sum_h[-1])
     plt.close()
     if make_fig_lock is not None:
         make_fig_lock.release()
