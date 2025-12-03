cddfdc12cee16cd3903fee8f9edfa9d606438076
ecnu_pc <821278796@qq.com>
2025-12-03 22:42:57 +0800

    Update package 0.0.3.181

--- jacksung/ai/utils/goes.py
+++ jacksung/ai/utils/goes.py
@@ -117,12 +117,11 @@ def get_filename_by_date_from_dir(dir_path, date, satellite='G18'):
     return file_lists
 
 
-def getNPfromDir(dir_path, date, satellite='G18', lock=None, return_coord=False):
+def getNPfromDir(dir_path, date, satellite='G18', lock=None, return_coord=False, infos=None):
     np_data = None
     coord = None
     data_channel_count = 0
     files = get_filename_by_date_from_dir(dir_path, date, satellite)
-    infos = None
     for channel, file in files.items():
         if infos is None:
             infos = get_resample_infos(os.path.join(dir_path, file), lock=lock)
