687efde0ccddd545d76da36650c7375142b011dc
ecnu_pc <821278796@qq.com>
2025-12-29 20:36:12 +0800

    Update package 0.0.4.51

--- jacksung/ai/utils/util.py
+++ jacksung/ai/utils/util.py
@@ -165,7 +165,6 @@ def parse_config(config=None, set_gpu=True):
 
         # select active gpu devices
         if args.gpu_ids is not None and torch.cuda.is_available():
-            print('use cuda & cudnn for acceleration!')
             print('the gpu id is: {}'.format(args.gpu_ids))
             device = torch.device('cuda')
             # device = torch.device('cuda:' + str(args.gpu_ids[0]))
@@ -205,92 +204,6 @@ def data_augmentation(images):
     if random.random() > 0.5:
         images = [torch.flip(image, [3]) for image in images]
     return images
-
-
-def make_fig(file_name, root_path, out_folder=None, tz='UTC',
-             colors=((0, '#1E90FF'), (0.1, '#1874CD'), (0.2, '#3A5FCD'), (0.3, '#0000CD'), (1, '#9400D3')),
-             area=((100, 140, 10), (20, 60, 10)), font_size=20, corp=(0, 0, None, None),
-             zoom_rectangle=(310 * 5, 300 * 5, 50 * 5, 40 * 5), docker=(300, 730), dpi=500, filter=0.3, exposure=None):
-    # corp = [92, 31, 542, 456]
-    print('过期的make_figure方法！')
-    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-    extents = [100, 140, 20, 60]
-    proj = ccrs.PlateCarree()
-    fig = plt.figure(dpi=dpi)
-    ax = fig.add_subplot(111, projection=proj)
-    ax.set_extent(extents, crs=proj)
-    # 读取TIFF数据
-    elevation = None
-    if type(file_name) == list:
-        for each_file in file_name:
-            file_path = os.path.join(root_path, each_file)
-            with rasterio.open(file_path) as dataset:
-                el_rd = dataset.read(1)
-                elevation[elevation < filter] = np.nan
-                if elevation is None:
-                    elevation = el_rd
-                else:
-                    elevation += el_rd
-    else:
-        file_path = os.path.join(root_path, file_name)
-        with rasterio.open(file_path) as dataset:
-            elevation = dataset.read(1)
-        elevation[elevation <= filter] = np.nan
-    elevation, colors = _get_color_normalization(elevation, colors)
-    cmap = LinearSegmentedColormap.from_list('custom_cmap', colors)
-    # 添加各种特征
-    land = cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='black', facecolor='none',
-                                        linewidth=0.4)
-    ax.add_feature(land)
-    ax.imshow(elevation, origin='upper', extent=extents, transform=proj, cmap=cmap)
-    ax.add_feature(cfeature.OCEAN)
-    ax.add_feature(cfeature.LAND)
-    # ax.add_feature(cfeature.LAKES, edgecolor='black')
-    ax.add_feature(cfeature.RIVERS)
-    # ax.add_feature(cfeature.BORDERS)
-    # 添加网格线
-    # ax.gridlines(linestyle='--')
-    # 设置大刻度和小刻度
-    tick_proj = ccrs.PlateCarree()
-    ax.set_xticks(np.arange(area[0][0], area[0][1] + 1, area[0][2]), crs=tick_proj)
-    # ax.set_xticks(np.arange(-180, 180 + 30, 30), minor=True, crs=tick_proj)
-    ax.set_yticks(np.arange(area[1][0], area[1][1] + 1, area[1][2]), crs=tick_proj)
-    # ax.set_yticks(np.arange(-90, 90 + 15, 15), minor=True, crs=tick_proj)
-    # 利用Formatter格式化刻度标签
-    ax.xaxis.set_major_formatter(LongitudeFormatter())
-    ax.yaxis.set_major_formatter(LatitudeFormatter())
-    if out_folder is None:
-        if type(file_name) == list:
-            file_name = file_name[0]
-            file_dir = 'concate'
-        else:
-            file_dir = 'outs'
-    else:
-        file_dir = out_folder
-    os.makedirs(os.path.join(root_path, file_dir), exist_ok=True)
-    file_name = file_name.replace('.tif', '.png')
-    file_title = datetime.strptime(file_name.split('-')[0].replace('target_', ''), '%Y%m%d_%H%M%S')
-    file_name = file_title.strftime('%Y%m%d_%H%M%S.png')
-    exposure = exposure if exposure else (60 if file_dir == 'concate' else 15)
-    if tz == 'BJT':
-        exposure_end = (file_title + timedelta(hours=8) + timedelta(minutes=exposure)).strftime('%H:%M')
-        file_title = (file_title + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M')
-    else:
-        exposure_end = (file_title + timedelta(minutes=exposure)).strftime('%H:%M')
-        file_title = file_title.strftime('%Y-%m-%d %H:%M')
-    ax.set_title(file_title + f'-' + exposure_end + f' ({tz})', fontsize=font_size)
-    plt.xticks(fontsize=font_size)
-    plt.yticks(fontsize=font_size)
-    # plt.title(fontsize=font_size)
-    file_save_path = os.path.join(root_path, file_dir, file_name)
-    plt.savefig(file_save_path)
-    if zoom_rectangle is not None:
-        read_png = cv2.imread(file_save_path)
-        read_png = zoomAndDock(read_png, zoom_rectangle, docker, scale_factor=5, border=14)
-        cv2.imwrite(file_save_path, read_png)
-    crop_png(file_save_path, left=corp[0], top=corp[1], right=corp[2], bottom=corp[3])
-    return file_save_path
-    # plt.show()
 
 
 def clipSatelliteNP(np_data, ld, area=((100, 140, 10), (20, 60, 10))):
